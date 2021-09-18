30bcd6098150336311e370c68dabb1ba35265ca5
swift_file <sky.y.m.318@gmail.com>
2021-09-18 23:45:36 +0900

    add gitkraken install

--- setup02.sh
+++ setup02.sh
@@ -1,2 +1,5 @@
 sudo snap install clion --classic
 sudo apt install guake -y
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+rm ./gitkraken-amd64.deb

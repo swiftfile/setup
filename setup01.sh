2908bfbdad8a0d588bd5b66b1a1775cba55113f7
swift_file <sky.y.m.318@gmail.com>
2021-09-19 00:42:55 +0900

    mod update kernel

--- setup01.sh
+++ setup01.sh
@@ -3,6 +3,7 @@ LANG=C xdg-user-dirs-gtk-update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm ./google-chrome-stable_current_amd64.deb
+sudo apt install linux-generic-hwe-20.04-edge -y
 sudo apt install snap -y
 sudo apt install curl -y 
 sudo apt install -y fcitx fcitx-mozc

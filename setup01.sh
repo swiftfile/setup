02627cee0beb5c99d84626fd97d7ae2322c6f5c8
swift_file <sky.y.m.318@gmail.com>
2021-09-18 23:43:17 +0900

    first commit

---
+++ setup01.sh
@@ -0,0 +1,7 @@
+sudo apt install vim -y
+LANG=C xdg-user-dirs-gtk-update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+sudo rm ./google-chrome-stable_current_amd64.deb
+sudo apt install snap -y
+sudo apt install curl -y 
---
+++ setup02.sh
@@ -0,0 +1,2 @@
+sudo snap install clion --classic
+sudo apt install guake -y

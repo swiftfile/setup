680cc0f4390ce1f9757e701098d6e3e8768b05de
Yusuke Mizoguchi <33048729+swiftfile@users.noreply.github.com>
2022-05-17 21:56:59 +0900

    not install chrome

--- setup01.sh
+++ setup01.sh
@@ -1,7 +1,7 @@
 sudo apt install vim -y
 LANG=C xdg-user-dirs-gtk-update
 sudo timedatectl set-local-rtc true
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo apt install ./google-chrome-stable_current_amd64.deb -y
 #sudo rm ./google-chrome-stable_current_amd64.deb
 sudo apt install linux-generic-hwe-20.04-edge -y

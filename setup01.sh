f8b503207b93a71129caf9b0b4e0c996db59a36b
Yusuke Mizoguchi <33048729+swiftfile@users.noreply.github.com>
2022-05-17 21:36:35 +0900

    chromeをinstallしないように

--- setup01.sh
+++ setup01.sh
@@ -2,8 +2,8 @@ sudo apt install vim -y
 LANG=C xdg-user-dirs-gtk-update
 sudo timedatectl set-local-rtc true
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
-sudo rm ./google-chrome-stable_current_amd64.deb
+#sudo apt install ./google-chrome-stable_current_amd64.deb -y
+#sudo rm ./google-chrome-stable_current_amd64.deb
 sudo apt install linux-generic-hwe-20.04-edge -y
 sudo apt install snap -y
 google-chrome https://slack.com/intl/ja-jp/downloads/instructions/ubuntu
